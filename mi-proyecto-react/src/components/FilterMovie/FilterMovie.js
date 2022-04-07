70fe4ec65416b6ebc4ee8a6e22667acc75647cd5
Agostina-Maldonado <you@example.com>
2022-04-07 10:42:09 -0300

    FilterMovie

---
+++ mi-proyecto-react/src/components/FilterMovie/FilterMovie.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class FilterMovie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {valor: ''};
+      }
+    
+      evitarSubmit(event) {
+        event.preventDefault();
+      }
+    
+      controlarCambios(event) {
+        this.setState({valor: event.target.value},
+            this.props.filtrarPeliculas(this.state.valor));
+      }
+    
+      render() {
+        return (
+         <form onSubmit={(event)=>this.evitarSubmit(event)}>
+           <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} />
+           <input type="submit" value="Submit" />
+         </form>
+        );
+        }
+}    
+
+export default FilterMovie; 
--- mi-proyecto-react/src/components/Header/Header.js
+++ mi-proyecto-react/src/components/Header/Header.js
@@ -8,10 +8,7 @@ function Header(){
         <section>
             <i class="fas fa-th"></i>
             <i class="fas fa-align-justify"></i>
-            <form action="">
-                <input type="text" name="search" id="" placeholder="Search">
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </form>
+            <FilterField filtrarPeliculas={(textoAEncontrar)=> this.filtrarPeliculas(textoAEncontrar)}/>
         </section>
     </header>
     )
