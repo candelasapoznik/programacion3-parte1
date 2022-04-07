91c4f36a34f554c892fe6e4285fe82d669aceb41
Your Name <you@example.com>
2022-04-07 10:18:36 -0300

    footer

---
+++ mi-proyecto-react/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(){
+    return(
+    <ul>
+        <li>Candela Sapoznik</li>
+        <li>Matias Maldonado</li>
+        <li>Agostina Maldonado</li>
+    </ul>
+    )
+}
+
+export default Footer
