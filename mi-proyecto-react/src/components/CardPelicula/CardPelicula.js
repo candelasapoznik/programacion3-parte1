5fd3368e51e2146f6eeb75e6e4e30cf616fa54aa
Agostina-Maldonado <you@example.com>
2022-04-16 15:26:09 -0300

    ver más y ver menos

--- mi-proyecto-react/src/components/CardPelicula/CardPelicula.js
+++ mi-proyecto-react/src/components/CardPelicula/CardPelicula.js
@@ -7,19 +7,18 @@ class CardPelicula extends Component{
         this.state = {
             viewMore: true,
             text: "Ver más",
-            infoAdicionalTarjeta: ""
         }
     }
     viewMore(){
         if(this.state.viewMore){
             this.setState({
                 viewMore: false,
-                text: this.state.text
+                text: "ver Menos"
             })
         } else {
             this.setState({
                 viewMore: true,
-                text: 'Ver menos'
+                text: this.state.text
             })            
         }
     }
@@ -29,10 +28,24 @@ class CardPelicula extends Component{
             <div className= 'movie-card'>
                 <img src={`https://image.tmdb.org/t/p/original${this.props.info.poster_path}`}alt="" />
                 <h4>{this.props.info.title}</h4>
-                <button className='more' onClick={()=>this.viewMore()}>{this.state.text}</button>
                 <p> {this.props.info.overview} </p>
-                
                 <button className='borrae' onClick={()=>this.props.removerTarjetas(this.props.info.id)}>Borrar</button> {/* Aquí onClick necesita retornar la función con el parámetro pero no es necesario pasar el parámentro dentro de los () de la arrow functions. La arrow está para ejecutar el método que ya tiene el dato que necesita como parámetro.  */}
+                <button className='more' onClick={()=>this.viewMore()}>{this.state.text}</button>
+                {this.state.viewMore == true ?
+                <p>  </p> :
+                <section className={`${this.state.viewMore}`}>
+                    <p>
+                    Release date: {this.props.info.release_date}
+                    </p>
+                    <p>
+                    Vote average: {this.props.info.vote_average}
+                    </p>
+                    <p>
+                    Popularity: {this.props.info.popularity}
+                    </p>
+                </section>}
+                
+                
             </div>
             
         );
